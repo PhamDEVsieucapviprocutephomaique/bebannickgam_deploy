3714bcb793a08eb95fb46f91866c05fff81f5354
phamdevctpmq <phamminhduc0865713395@gmail.com>
2025-11-01 03:33:57 +0700

    fix error databseurl

--- core/database.py
+++ core/database.py
@@ -1,7 +1,12 @@
 from sqlmodel import SQLModel, create_engine, Session
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print(f"DEBUG: DATABASE_URL value is: {DATABASE_URL}")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
